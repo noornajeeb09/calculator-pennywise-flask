fa547a8f06ca61482a88ecf195aecd27e209deba
Snigdhaaaa <snigdha10.sd@gmail.com>
2021-07-26 18:39:02 +0000

    started database

--- app.py
+++ app.py
@@ -4,16 +4,26 @@ from flask import Flask, render_template, request
 from datetime import datetime
 # from model import getImageUrlFrom
 import os
+from flask_pymongo import PyMongo
 
 # -- Initialization section --
 app = Flask(__name__)
 
+#name of database
+app.config['MONGO_DBNAME']= 'pennywise-database'
+#URI of database
+app.config['MONGO_URI']='mongodb+srv://new-admin:DYuiP04unSez1wQb@cluster0.btlah.mongodb.net/pennywise-database?retryWrites=true&w=majority'
+mongo= PyMongo(app)
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
+    #connect to appropriate "collection" in database
+    users = mongo.db.users
+    users.insert_one({"name":"Snigdha", "password":"blah"})
     return render_template("index2.html", time = datetime.now())
+
 
 @app.route('/calculate', methods = ['POST', 'GET'])
 def calculate():
@@ -27,3 +37,5 @@ def calculate():
     monthly_amount_to_save = (goal_to_save - (current_savings + annual_amount_left))/12
     
     return (str(round(monthly_amount_to_save)))
+
+
