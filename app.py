1a1c9f8a74d9bc874a842b9f53a2b7eec25147d0
noornajeeb09 <noor.arfeen1@gmail.com>
2021-07-26 15:31:15 +0000

    first upload

--- app.py
+++ app.py
@@ -14,3 +14,17 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/calculate', methods = ['POST', 'GET'])
+def calculate():
+    current_savings = int(request.form['diffname1'])
+    monthly_income = int(request.form['2'])
+    monthly_expenses = int(request.form['3'])
+    goal_to_save = int(request.form['gifchoice'])
+
+    monthly_amount_left = int(monthly_income - monthly_expenses) 
+    annual_amount_left = (monthly_amount_left * 12)
+    monthly_amount_to_save = (goal_to_save - (current_savings + annual_amount_left))/12
+    
+    return (str(round(monthly_amount_to_save)))
+
